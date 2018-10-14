eecae411bd39a24f565297d3c381a33ae42efde5
Robin Simonklein <rsimonklein@gmail.com>
2018-10-14 15:49:35 +0200

    Deploy test

---
+++ dist/precache-manifest.c5c956e9a94f476bac20ef7bbf58fd54.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "93481e4f603eb7dcd721",
+    "url": "/js/chunk-vendors.b77728b8.js"
+  },
+  {
+    "revision": "1f97995c405e125b718d",
+    "url": "/js/app.28dad418.js"
+  },
+  {
+    "revision": "f54bb8a69f4d9cb88500126806dc260e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/img/weathericons-regular-webfont.ecaf8b48.svg"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/fonts/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/fonts/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/fonts/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/fonts/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "1f97995c405e125b718d",
+    "url": "/css/app.9f49a29e.css"
+  }
+];
---
+++ dist/service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.c5c956e9a94f476bac20ef7bbf58fd54.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "meteo"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
